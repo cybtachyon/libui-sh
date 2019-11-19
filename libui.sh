0d352e5e94882b888aa8d2030f04821ab08e5bee
Derek "tachyon" Reese <cyb.tachyon@gmail.com>
2019-11-18 19:57:38 -0800

    Adds progress bar function

--- libui.sh
+++ libui.sh
@@ -172,6 +172,19 @@ log ()
 	done
 }
 
+# display a progress bar
+bar() {
+    # Usage: bar 1 10
+    #            ^----- Elapsed Percentage (0-100).
+    #               ^-- Total length in chars.
+    ((elapsed=$1*$2/100))
+
+    # Create the bar with spaces.
+    printf -v prog  "%${elapsed}s"
+    printf -v total "%$(($2-elapsed))s"
+
+    printf '%s\r' "[${prog// /-}${total}]"
+}
 
 # $1 = one or more debug categories (must be in a list of specified categories. see init function) (separated by spaces)
 #      Useful when grepping in the logfile
